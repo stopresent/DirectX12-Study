2181318007680033160791f10820560db4ed1b74
stopresent <86364202+stopresent@users.noreply.github.com>
2023-12-05 22:56:59 +0900

    [ADD] 프로젝트 세팅 완

--- DirectX12Game/Client/pch.h
+++ DirectX12Game/Client/pch.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <memory>
-using namespace std;
+#pragma comment(lib, "Engine.lib")
+
+#include "EnginePch.h"
--- DirectX12Game/Engine/EnginePch.h
+++ DirectX12Game/Engine/EnginePch.h
@@ -42,3 +42,5 @@ using Vec2		= XMFLOAT2;
 using Vec3		= XMFLOAT3;
 using Vec4		= XMFLOAT4;
 using Matrix	= XMMATRIX;
+
+void HelloEngine();
